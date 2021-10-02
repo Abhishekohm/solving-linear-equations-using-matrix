a15871c2b6db2f9680139abd8affbb8cc7156dbf
Abhishek <abhishek.9867.338961@gmail.com>
2021-10-02 15:26:15 +0530

    logic done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,46 @@
 import React, { useRef, useState } from "react";
 import guassJacobi from "../utility/GaussJacobi";
+import guassSiedel from "../utility/guassSiedel";
 import Entry from "./Entry";
+
+const GJ = "gaussJacobi";
+const GS = "guassSiedel";
 
 const Form = () => {
   const formRef = useRef();
   const iterations = useRef();
   const precision = useRef();
   const [entries, setEntries] = useState([]);
+  const [x0, setX0] = useState();
+  const [y0, setY0] = useState();
+  const [z0, setZ0] = useState();
+  const [algo, setAlgo] = useState("");
+
   // formSubmit Handler
   const handleSubmit = (e) => {
     e.preventDefault();
     const input = [];
-    for (let child of formRef.current.children) {
-      const arr = [];
+    for (let child of formRef.current.children[0].children) {
+      let arr = [];
       for (let child1 of child.children) {
         arr.push(parseInt(child1.value));
       }
       input.push([...arr]);
     }
-    console.log(input);
+
     const callObj = {
       input,
       iterations: parseInt(iterations.current.value),
       precision: parseInt(precision.current.value),
+      x0,
+      y0,
+      z0,
     };
-    console.log(callObj);
-    const res = guassJacobi(callObj);
-    console.log(res);
-    setEntries(res);
+    if (algo === GJ) {
+      setEntries(guassJacobi(callObj));
+    } else {
+      setEntries(guassSiedel(callObj));
+    }
   };
   return (
     <>
@@ -36,23 +49,25 @@ const Form = () => {
           <h1>Guass Jacobi Method</h1>
         </div>
         <form ref={formRef} onSubmit={handleSubmit}>
-          <div>
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
-          </div>
-          <div>
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
-          </div>
-          <div>
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
+          <div className="input-ctn">
+            <div>
+              <input type="number" required />
+              <input type="number" required />
+              <input type="number" required />
+              <input type="number" required />
+            </div>
+            <div>
+              <input type="number" required />
+              <input type="number" required />
+              <input type="number" required />
+              <input type="number" required />
+            </div>
+            <div>
+              <input type="number" required />
+              <input type="number" required />
+              <input type="number" required />
+              <input type="number" required />
+            </div>
           </div>
           <div className="precision-ctn">
             <input
@@ -65,6 +80,48 @@ const Form = () => {
               placeholder="No. of digits after decimal"
               ref={precision}
             />
+          </div>
+          <div>
+            <div>
+              <label>
+                <b>Enter initial values:</b>
+              </label>
+            </div>
+            <div>
+              <input
+                type="number"
+                placeholder="x0"
+                onChange={(e) => {
+                  setX0(e.target.value);
+                }}
+                value={x0}
+              />
+              <input
+                type="number"
+                placeholder="y0"
+                onChange={(e) => {
+                  setY0(e.target.value);
+                }}
+                value={y0}
+              />
+              <input
+                type="number"
+                placeholder="z0"
+                onChange={(e) => {
+                  setZ0(e.target.value);
+                }}
+                value={z0}
+              />
+            </div>
+          </div>
+          <div className="algo">
+            <label htmlFor="algo">
+              <b>Select Method</b>
+            </label>
+            <select id="algo" onChange={(e) => setAlgo(e.target.value)}>
+              <option value={GJ}>Guass Jacobi</option>
+              <option value={GS}>Guass Siedel</option>
+            </select>
           </div>
           <button type="submit">Submit</button>
         </form>
--- src/utility/guassSiedel.js
+++ src/utility/guassSiedel.js
@@ -31,7 +31,7 @@ const change = (input) => {
   const r3 = input[2];
   input[c1] = [...r1];
   input[c2] = [...r2];
-  input[c3] = [...r3]; // ... spread operator
+  input[c3] = [...r3]; // ...spread operator
 };
 
 const equals = (x0y0z0, x1y1z1) => {
@@ -49,35 +49,31 @@ const copy = (x0y0z0, x1y1z1) => {
   return;
 };
 
-const guassJacobi = ({ input, iterations, precision, x0, y0, z0 }) => {
+const guassSiedel = ({ input, x0, y0, z0, iterations, precision }) => {
   const res = check(input);
-  console.log(iterations, precision);
+  console.log(input, x0, y0, z0, iterations, precision);
   if (res === false) {
     change(input);
   }
   let x0y0z0 = [x0 ? x0 : 0, y0 ? y0 : 0, z0 ? z0 : 0];
   let intermediateRes = [];
   let itr = iterations ? iterations : 10;
-
-  let precisionSetter = 1;
-  precision = precision ? precision : 3;
-
-  for (let i = 0; i < precision; i++) {
-    precisionSetter = precisionSetter * 10;
-  }
+  precision = precision ? precision : 4;
+  let pwer = 10 ** precision;
+  console.log(itr, pwer);
 
   while (itr--) {
     let x1y1z1 = [];
-    for (let i = 0; i < 3; i++) {
-      let num = 0;
-      for (let j = 0; j < 3; j++) {
-        if (i !== j) num += input[i][j] * x0y0z0[j];
-      }
-      num = input[i][3] - num;
-      num = num / input[i][i];
-      num = Math.round(num * precisionSetter) / precisionSetter;
-      x1y1z1.push(num);
-    }
+    let x1 =
+      (input[0][3] - input[0][1] * x0y0z0[1] - input[0][2] * x0y0z0[2]) /
+      input[0][0];
+    x1 = Math.round(x1 * pwer) / pwer;
+    let y1 =
+      (input[1][3] - input[1][0] * x1 - input[1][2] * x0y0z0[2]) / input[1][1];
+    y1 = Math.round(y1 * pwer) / pwer;
+    let z1 = (input[2][3] - input[2][0] * x1 - input[2][1] * y1) / input[2][2];
+    z1 = Math.round(z1 * pwer) / pwer;
+    x1y1z1.push(x1, y1, z1);
     const res = equals(x0y0z0, x1y1z1);
     if (res === true) {
       break;
@@ -86,8 +82,9 @@ const guassJacobi = ({ input, iterations, precision, x0, y0, z0 }) => {
       intermediateRes.push([...x0y0z0]);
     }
   }
-  console.log(intermediateRes);
   return [...intermediateRes];
 };
 
-export default guassJacobi;
+// guassSiedel(input);
+
+export default guassSiedel;
